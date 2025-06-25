4b99cad15aa738ee40ce2c73f53c8b35fd4c9f44
karramb <ap.vecna@gmail.com>
2025-06-25 22:19:08 +0300

    fix docker

--- monitor/monitor/settings.py
+++ monitor/monitor/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = True
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1').split()
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1').split(",")
 
 AUTH_USER_MODEL = 'users.ArkanUser'
 
